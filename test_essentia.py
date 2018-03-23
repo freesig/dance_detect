e3652959123327a474607afd7991da46eb3ecf1a
Tom Gowan <s3549186@student.rmit.edu.au>
2018-03-23 14:35:06 +1100

    beat detection

--- test_essentia.py
+++ test_essentia.py
@@ -1,6 +1,30 @@
+import sys
 import essentia
 
 import essentia.standard
 import essentia.streaming
 
-print dir(essentia.standard)
+from essentia.streaming import MonoLoader
+from essentia.streaming import VectorInput
+from essentia.streaming import BeatTrackerDegara
+from essentia.standard import FrameGenerator
+
+#print dir(essentia.standard)
+
+if len(sys.argv) < 2:
+    print("Supports 16 bit wave file. Use %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
+loader = essentia.standard.MonoLoader(filename = sys.argv[1])()
+beat_tracker = BeatTrackerDegara()
+
+pool = essentia.Pool()
+print "made it"
+for frame in FrameGenerator(loader, frameSize = 100*1024, hopSize = 512, startFromZero=True):
+    v_in = VectorInput(frame)
+    v_in.data >> beat_tracker.signal
+    beat_tracker.ticks >> (pool, 'Rhythm.ticks')
+
+    essentia.run(v_in)
+
+    print "beats: ", pool['Rhythm.ticks']
+
