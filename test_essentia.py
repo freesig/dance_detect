6665d9d3986bf06eb37d711797b14c3e4fd74370
Tom Gowan <s3549186@student.rmit.edu.au>
2018-03-23 17:22:14 +1100

    basic bpm checker

--- test_essentia.py
+++ test_essentia.py
@@ -4,27 +4,27 @@ import essentia
 import essentia.standard
 import essentia.streaming
 
-from essentia.streaming import MonoLoader
 from essentia.streaming import VectorInput
-from essentia.streaming import BeatTrackerDegara
-from essentia.standard import FrameGenerator
-
-#print dir(essentia.standard)
+from essentia.streaming import RhythmExtractor2013
+from essentia.standard import FrameGenerator 
 
 if len(sys.argv) < 2:
     print("Supports 16 bit wave file. Use %s filename.wav" % sys.argv[0])
     sys.exit(-1)
 loader = essentia.standard.MonoLoader(filename = sys.argv[1])()
-beat_tracker = BeatTrackerDegara()
 
-pool = essentia.Pool()
-print "made it"
 for frame in FrameGenerator(loader, frameSize = 100*1024, hopSize = 512, startFromZero=True):
     v_in = VectorInput(frame)
+    beat_tracker = RhythmExtractor2013(method="degara")
+    pool = essentia.Pool()
     v_in.data >> beat_tracker.signal
     beat_tracker.ticks >> (pool, 'Rhythm.ticks')
-
+    beat_tracker.bpm >> (pool, 'Rhythm.bpm')
+    beat_tracker.confidence >> None 
+    beat_tracker.estimates >> None 
+    beat_tracker.bpmIntervals >> None 
     essentia.run(v_in)
 
     print "beats: ", pool['Rhythm.ticks']
+    print "bpm: ", pool['Rhythm.bpm']
 
