3b9105ebc784489fe8710458185fd62489457c82
Tom Gowan <s3549186@student.rmit.edu.au>
2018-05-08 16:01:19 +1000

    parralell

--- test_essentia.py
+++ test_essentia.py
@@ -8,6 +8,9 @@ import socket
 import threading
 import Queue
 
+
+from multiprocessing.pool import ThreadPool
+
 import time
 
 import wave
@@ -18,12 +21,14 @@ import essentia.streaming
 from essentia.streaming import VectorInput
 from essentia.streaming import RhythmExtractor2013
 from essentia.standard import Energy 
+from essentia.streaming import OnsetRate
 from essentia.standard import FrameGenerator 
 UDP_IP = "10.42.1.254"
-UDP_PORT = [55000, 54000, 53000]
+UDP_PORT = [55000, 54000, 52000]
 LENGTH = 100
 CHUNK = 1024 * LENGTH 
 FORMAT = pyaudio.paFloat32
+
 
 class ExtractionThread(threading.Thread):
     def __init__(self, ed, ps):
@@ -31,8 +36,10 @@ class ExtractionThread(threading.Thread):
         self.extract_done = ed
         self.play_started = ps
         self.stoprequest = threading.Event()
+    
 
     def run(self):
+
         loader = essentia.standard.MonoLoader(filename = sys.argv[1])()
 
         msg = "0"
@@ -50,45 +57,70 @@ class ExtractionThread(threading.Thread):
                 self.play_started.get()
             
             start_time =  time.time()
-
-            v_in = VectorInput(frame)
-            beat_tracker = RhythmExtractor2013(method="degara")
+            pool2 = essentia.Pool()
             pool = essentia.Pool()
-            v_in.data >> beat_tracker.signal
-            beat_tracker.ticks >> (pool, 'Rhythm.ticks')
-            beat_tracker.bpm >> (pool, 'Rhythm.bpm')
-            beat_tracker.confidence >> None 
-            beat_tracker.estimates >> None 
-            beat_tracker.bpmIntervals >> None 
-            essentia.run(v_in)
+            def beat_call():
+                v_in = VectorInput(frame)
+                beat_tracker = RhythmExtractor2013(method="degara")
+                v_in.data >> beat_tracker.signal
+                beat_tracker.ticks >> (pool, 'Rhythm.ticks')
+                beat_tracker.bpm >> (pool, 'Rhythm.bpm')
+                beat_tracker.confidence >> None 
+                beat_tracker.estimates >> None 
+                beat_tracker.bpmIntervals >> None 
+                essentia.run(v_in)
+
+            def onset_call():
+                v_in2 = VectorInput(frame)
+                onset = OnsetRate()
+                v_in2.data >> onset.signal 
+                onset.onsetRate >> (pool2, 'Rhythm.onsetRate')
+                onset.onsetTimes >> None
+                essentia.run(v_in2)
+            
+            beat_thread = threading.Thread(target=beat_call, args=())
+            onset_thread = threading.Thread(target=onset_call, args=())
+            beat_thread.daemon = True
+            onset_thread.daemon = True
+            #print "time1: ", time.time()
+            beat_thread.start()
+            #print "time2: ", time.time()
+            onset_thread.start()
+            #print "time3: ", time.time()
+
 
             energy = Energy()
             frame_energy = energy(frame)
+            beat_thread.join()
+            onset_thread.join()
+
 
             bpm = pool['Rhythm.bpm']
             spb = 60.0 / bpm if bpm > 0 else 0.0
             look_ahead_n = 16
             beats = pool['Rhythm.ticks']
+            onset = pool2['Rhythm.onsetRate']
 
             for i, b in enumerate(beats):
                 beat_count += 1
-                print "bc: ", beat_count
                 if(beat_count % N_BEATS == 0):
                     next_beat = start_time + b + spb * look_ahead_n
-                    print "detected: ", i, b
 
                     for i, sock in enumerate(socks):
-                        sock.sendto(str(next_beat) + "," + str(frame_energy), (UDP_IP, UDP_PORT[i]))
+                        sock.sendto(str(next_beat) + "," + str(frame_energy) + "," + str(onset),
+                                (UDP_IP, UDP_PORT[i]))
                     
 
-            
+            '''
             print "beats: ", pool['Rhythm.ticks']
             print "energy: ", frame_energy 
+            print "onset: ", pool2['Rhythm.onsetRate']
             print "bpm: ", pool['Rhythm.bpm']
             print "spb: ", spb
             print "bar started: ", start_time
             print "time now: ", time.time()
             print "next_beat: ", next_beat
+            '''
             self.extract_done.put(True)
 
     def stop(self):
